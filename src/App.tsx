06da9c2d6a76449c22f678c4f08656f0273070a6
federicoPedraza <federico.pedraza4@gmail.com>
2023-01-20 18:16:55 -0300

    added validations and translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Canvas } from './components/canvas/canvas';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './translations/i18n';
 
 function App() {
   return (
-    <div className='full-screen'>
-      <Canvas />
-    </div>
+    <I18nextProvider i18n={i18n}>
+      <div className='full-screen'>
+          <Canvas />
+      </div>
+    </I18nextProvider>
   );
 }
 
--- src/components/cards/signup/signup.card.tsx
+++ src/components/cards/signup/signup.card.tsx
@@ -1,35 +1,34 @@
 import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next';
 import RestService from '../../../services/rest.service';
-import { EMAIL_VERIFICATION_REGEX, isEmptyOrNull, PASSWORD_VERIFICATION_REGEX, USERNAME_VERIFICATION_REGEX } from '../../../utils/verifiers';
+import { EMAIL_VERIFICATION_REGEX, PASSWORD_VERIFICATION_REGEX, USERNAME_VERIFICATION_REGEX } from '../../../utils/verifiers';
 import { Card } from '../card/card';
 import { ICard } from '../card/card.constants';
 import './signup.card.sass';
 
 const SignupCard = (props: ICard) => {
+    const { t } = useTranslation();
     const [ username, setUsername ] = useState<string>('');
     const [ email, setEmail ] = useState<string>('');
     const [ password, setPassword ] = useState<string>('');
-    const [ message, setMessage ] = useState<string>('Please fill all fields.');
+    const [ message , setAlertMessage ] = useState<string>('');
 
     const handleSignup = async (event: any) => {
         event.preventDefault();
-        
-        if (isEmptyOrNull(password) || isEmptyOrNull(email) || isEmptyOrNull(username)) {
-            setMessage("Please fill all fields");
+
+        // Validation methods
+        if (!USERNAME_VERIFICATION_REGEX.test(username)) {
+            setAlertMessage(t('card.signup.errors.invalid_username') as string)
+            return;
         }
 
         if (!EMAIL_VERIFICATION_REGEX.test(email)) {
-            setMessage("Your email is invalid or unavailable");
+            setAlertMessage(t('card.signup.errors.invalid_email') as string)
             return;
         }
-        
-        if (USERNAME_VERIFICATION_REGEX.test(username)) {
-            setMessage("Your username has invalid characters. It must contain at least 3 characters");
-            return;
-        }
-        
-        if (PASSWORD_VERIFICATION_REGEX.test(password)) {
-            setMessage("Your password is invalid. It must contain at least 8 characters, one capital letter, and one number.");
+
+        if (!PASSWORD_VERIFICATION_REGEX.test(password)) {
+            setAlertMessage(t('card.signup.errors.invalid_password') as string)
             return;
         }
 
@@ -45,22 +44,22 @@ const SignupCard = (props: ICard) => {
     return (
         <Card {...props} title="Signup" closeable>
             <div className='signup-container'>
-                <span>{message}</span>
-                <form onSubmit={handleSignup}>
+                <form className='signup-container' onSubmit={handleSignup}>
                     <label>
-                        Username
+                        {t('card.signup.username')}
                         <input type="text" value={username} required onChange={v => setUsername(v.target.value) } />
                     </label>
                     <label>
-                        Email
+                        {t('card.signup.email')}
                         <input type="email" value={email} required onChange={v => setEmail(v.target.value) } />
                     </label>
                     <label>
-                        Password
+                        {t('card.signup.password')}
                         <input type="password" value={password} required onChange={v => setPassword(v.target.value) }  />
                     </label>
                     <input type="submit" value="Signup" />
                 </form>
+                <span>{message}</span>
             </div>
         </Card>
     )
